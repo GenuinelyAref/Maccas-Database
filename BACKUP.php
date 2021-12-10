cd17c8d95da8ebbc3e58e7073f02d36d1c5510ab
GenuinelyAref <osmana69529@masseyhigh.school.nz>
2021-12-10 17:21:40 +1300

    general maintenance (add + edit)
    * edit index page to contain updated references
    
    * edit styling extensively to match changes to database. CSS changes include:
    - change template colours (text and background)
    - centre logo in its div
    - add styling to advanced search form and add-entry form, as well as all displayed entries
    
     * add function file to "clean" data
    
    * change logo to match website
    
    * download favicon files (not used)
    
    * create backup file to store a length of PHP in case of trialling failure

---
+++ BACKUP.php
@@ -0,0 +1,146 @@
+<!-- Total Fat -->
+<div class="flex-container">
+    <div class="adv-text">
+      Total Fat:
+    </div> <!-- close total fat label div-->
+
+    <div>
+      <select class="search adv" name="total_fat_label">
+        <option selected value="">Choose...</option>
+        <option value="At least">At least</option>
+        <option value="At most">At most</option>
+      </select>
+    </div> <!-- close total fat drop down div-->
+
+    <div>
+      <input class="adv adv-cost" type="text" name="total_fat" size="2"
+      value="" placeholder="" maxlength="4"/>
+    </div> <!-- close total fat amount div-->
+
+</div> <!-- close total fat div -->
+
+<!-- Saturated Fat -->
+<div class="flex-container">
+    <div class="adv-text">
+      Saturated Fat:
+    </div> <!-- close saturated fat label div-->
+
+    <div>
+      <select class="search adv" name="saturated_fat_label">
+        <option selected value="">Choose...</option>
+        <option value="At least">At least</option>
+        <option value="At most">At most</option>
+      </select>
+    </div> <!-- close saturated fat drop down div-->
+
+    <div>
+      <input class="adv adv-cost" type="text" name="saturated_fat" size="2"
+      value="" placeholder="" maxlength="4"/>
+    </div> <!-- close saturated fat amount div-->
+
+</div> <!-- close saturated fat div -->
+
+<!-- Cholesterol -->
+<div class="flex-container">
+    <div class="adv-text">
+      Cholesterol:
+    </div> <!-- close cholesterol label div-->
+
+    <div>
+      <select class="search adv" name="cholesterol_label">
+        <option selected value="">Choose...</option>
+        <option value="At least">At least</option>
+        <option value="At most">At most</option>
+      </select>
+    </div> <!-- close cholesterol drop down div-->
+
+    <div>
+      <input class="adv adv-cost" type="text" name="cholesterol" size="2"
+      value="" placeholder="" maxlength="4"/>
+    </div> <!-- close cholesterol amount div-->
+
+</div> <!-- close cholesterol div -->
+
+<!-- Carbohydrates -->
+<div class="flex-container">
+    <div class="adv-text">
+      Carbohydrates:
+    </div> <!-- close carbohydrates label div-->
+
+    <div>
+      <select class="search adv" name="carbohydrates_label">
+        <option selected value="">Choose...</option>
+        <option value="At least">At least</option>
+        <option value="At most">At most</option>
+      </select>
+    </div> <!-- close carbohydrates drop down div-->
+
+    <div>
+      <input class="adv adv-cost" type="text" name="carbohydrates" size="2"
+      value="" placeholder="" maxlength="4"/>
+    </div> <!-- close carbohydrates amount div-->
+
+</div> <!-- close carbohydrates div -->
+
+<!-- Dietary Fiber -->
+<div class="flex-container">
+    <div class="adv-text">
+      Dietary Fiber:
+    </div> <!-- close dietary fiber label div-->
+
+    <div>
+      <select class="search adv" name="dietary_fiber_label">
+        <option selected value="">Choose...</option>
+        <option value="At least">At least</option>
+        <option value="At most">At most</option>
+      </select>
+    </div> <!-- close dietary fiber drop down div-->
+
+    <div>
+      <input class="adv adv-cost" type="text" name="dietary_fiber" size="2"
+      value="" placeholder="" maxlength="4"/>
+    </div> <!-- close dietary fiber amount div-->
+
+</div> <!-- close dietary fiber div -->
+
+<!-- Sugars -->
+<div class="flex-container">
+    <div class="adv-text">
+      Sugars:
+    </div> <!-- close sugars label div-->
+
+    <div>
+      <select class="search adv" name="sugars_label">
+        <option selected value="">Choose...</option>
+        <option value="At least">At least</option>
+        <option value="At most">At most</option>
+      </select>
+    </div> <!-- close sugars drop down div-->
+
+    <div>
+      <input class="adv adv-cost" type="text" name="sugars" size="2"
+      value="" placeholder="" maxlength="4"/>
+    </div> <!-- close sugars amount div-->
+
+</div> <!-- close sugars div -->
+
+<!-- Protein -->
+<div class="flex-container">
+    <div class="adv-text">
+      Protein:
+    </div> <!-- close protein label div-->
+
+    <div>
+      <select class="search adv" name="protein_label">
+        <option selected value="">Choose...</option>
+        <option value="At least">At least</option>
+        <option value="At most">At most</option>
+      </select>
+    </div> <!-- close protein drop down div-->
+
+    <div>
+      <input class="adv adv-cost" type="text" name="protein" size="2"
+      value="" placeholder="" maxlength="4"/>
+    </div> <!-- close protein amount div-->
+
+</div> <!-- close protein div -->
--- index.php
+++ index.php
@@ -1,68 +1,22 @@
-<!DOCTYPE HTML>
+<?php include("topbit.php") ?>
 
-<html lang="en">
-    
-
-<head>
-    <meta charset="utf-8">
-    <meta name="description" content="games, apps, ">
-    <meta name="GTT" content="Game / App Database">
-    <meta name="keywords" content="games, apps, ratings">
-    
-    <title>Game Database</title>
-
-    <!-- for multiple fonts change | to %7c * no spaces*  -->
-    <link href="https://fonts.googleapis.com/css?family=Lato%7cUbuntu" rel="stylesheet">  
-
-    <link rel="stylesheet" href="css/font-awesome.min.css">
-    <link rel="stylesheet" href="css/data_style.css">    <!-- custom style sheet -->
-
-    
-</head>
-
-<body>
-    
-    <p class="message">Eek!  Your browser does not support grid.  Please upgrade your system.</p>
-        
-    <div class="wrapper">
-    
-        <!-- logo / small image goes here -->
-        <div class="box logo">
-            <a href="index.php"><img src="images/logo.png" width="261" height="150" alt="Dice" /></a>
-        </div> <!-- / logo -->
-        
-        <div class="box banner">
-            <h1>Games Database</h1>        
-        </div> <!-- / banner -->
-                       
-            
         <div class="box main">
             <h2>Welcome</h2>
-            
-            
+
+
             <p>
-                Put link to original dataset here
+                This database includes all the meals available in Mcdonald's menu at the time of download.
+                <h3>Acknowledgements & Sources</h3>
+                <ul>
+                    <li><a href="https://www.kaggle.com/mcdonalds/nutrition-facts">
+                    original "Nutrition Facts for Mcdonald's Menu" dataset</a></li>
+                    <li>Logo Image by <a href="https://pixabay.com/users/grafikacesky-947552/?utm_source=link-attribution&amp;utm_medium=referral&amp;utm_campaign=image&amp;utm_content=998495">grafikacesky</a> from <a href="https://pixabay.com/?utm_source=link-attribution&amp;utm_medium=referral&amp;utm_campaign=image&amp;utm_content=998495">Pixabay</a></li>
+                </ul>
             </p>
-            
 
-            
+
+
+
         </div> <!-- / main -->
-        
-        <div class="box side">
-           
-           <h2>Search Area</h2>
-           
-           <p>This is where the search area goes
 
-            
-        </div> <!-- / side bar -->
-        
-        <div class="box footer">
-            CC GTT 20XX
-        </div> <!-- / footer -->
-                
-        
-    </div> <!-- / wrapper -->
-    
-            
-</body>
+<?php include("bottombit.php") ?>
