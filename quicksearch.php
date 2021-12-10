51044fd4265da8de69519647644795cd0e9978bc
GenuinelyAref <osmana69529@masseyhigh.school.nz>
2021-12-10 17:23:16 +1300

    add basic search options
    * create input box to search by item name and its category
    
    * create quick-search option to search for items with no more than 200 calories

---
+++ quicksearch.php
@@ -0,0 +1,20 @@
+<?php include("topbit.php");
+
+      $find_sql = "SELECT * FROM `food_details`
+      JOIN category ON (food_details.CategoryID = category.CategoryID)
+      WHERE `Calories` <= 200 ORDER BY `Calories` DESC
+      ";
+      $find_query = mysqli_query($dbconnect, $find_sql);
+      $find_rs = mysqli_fetch_assoc($find_query);
+      $count = mysqli_num_rows($find_query);
+
+?>
+
+  <div class="box main">
+    <h2>All Results</h2>
+
+    <?php include "results.php"; ?>
+
+  </div> <!-- / main -->
+
+<?php include("bottombit.php") ?>
